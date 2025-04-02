2b8910b99b8ac1529b900efba7e43d60a0e828e1
Efrat Kelner <efratkelner@gmail.com>
2025-04-02 20:06:09 +0300

    Add Google Drive Upload

---
+++ DriveUpload/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+    using DriveUpload.Models;
+    using System.Web;
+    using System.Web.Mvc;
+
+    namespace DriveUpload.Controllers
+    {
+        public class HomeController : Controller
+        {
+            public ActionResult Index()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public ActionResult Index(HttpPostedFileBase file)
+            {
+                GoogleDriveAPIHelper.UplaodFileOnDrive(file);
+                ViewBag.Success = "File Uploaded on Google Drive";
+                return View();
+            }
+        }
+    }
